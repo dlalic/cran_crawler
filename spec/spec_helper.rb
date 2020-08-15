5d6a951d45a2ea519d2890b596b06b8a8faec6ad
Dunja Lalic <dunja.lalic@gmail.com>
2020-08-15 16:43:21 +0200

    Fix vcr hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :Typhoeus
+  c.hook_into :typhoeus
   c.configure_rspec_metadata!
   c.preserve_exact_body_bytes do |http_message|
     http_message.body.encoding.name == 'UTF-8'
