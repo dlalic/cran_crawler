d514b8486eb7edd64d65fe71f72bcd8f732420bf
Dunja Lalic <dunja.lalic@gmail.com>
2020-08-18 12:57:35 +0200

    Fix typo

--- lib/db/schema.rb
+++ lib/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,73 +10,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_817_110_239) do
+ActiveRecord::Schema.define(version: 2020_08_17_110239) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'authors', force: :cascade do |t|
-    t.date 'updated_at'
-    t.bigint 'user_id', null: false
-    t.bigint 'version_id', null: false
-    t.index %w[user_id version_id], name: 'index_authors_on_user_id_and_version_id', unique: true
-    t.index ['user_id'], name: 'index_authors_on_user_id'
-    t.index ['version_id'], name: 'index_authors_on_version_id'
+  create_table "authors", force: :cascade do |t|
+    t.date "updated_at"
+    t.bigint "user_id", null: false
+    t.bigint "version_id", null: false
+    t.index ["user_id", "version_id"], name: "index_authors_on_user_id_and_version_id", unique: true
+    t.index ["user_id"], name: "index_authors_on_user_id"
+    t.index ["version_id"], name: "index_authors_on_version_id"
   end
 
-  create_table 'dependencies', force: :cascade do |t|
-    t.date 'updated_at'
-    t.bigint 'package_id', null: false
-    t.index ['package_id'], name: 'index_dependencies_on_package_id', unique: true
+  create_table "dependencies", force: :cascade do |t|
+    t.date "updated_at"
+    t.bigint "package_id", null: false
+    t.index ["package_id"], name: "index_dependencies_on_package_id", unique: true
   end
 
-  create_table 'maintainers', force: :cascade do |t|
-    t.date 'updated_at'
-    t.bigint 'user_id', null: false
-    t.bigint 'version_id', null: false
-    t.index %w[user_id version_id], name: 'index_maintainers_on_user_id_and_version_id', unique: true
-    t.index ['user_id'], name: 'index_maintainers_on_user_id'
-    t.index ['version_id'], name: 'index_maintainers_on_version_id'
+  create_table "maintainers", force: :cascade do |t|
+    t.date "updated_at"
+    t.bigint "user_id", null: false
+    t.bigint "version_id", null: false
+    t.index ["user_id", "version_id"], name: "index_maintainers_on_user_id_and_version_id", unique: true
+    t.index ["user_id"], name: "index_maintainers_on_user_id"
+    t.index ["version_id"], name: "index_maintainers_on_version_id"
   end
 
-  create_table 'packages', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'checksum'
-    t.boolean 'indexed', default: false, null: false
-    t.date 'updated_at'
-    t.index ['name'], name: 'index_packages_on_name', unique: true
+  create_table "packages", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "checksum"
+    t.boolean "indexed", default: false, null: false
+    t.date "updated_at"
+    t.index ["name"], name: "index_packages_on_name", unique: true
   end
 
-  create_table 'suggestions', force: :cascade do |t|
-    t.date 'updated_at'
-    t.bigint 'package_id', null: false
-    t.index ['package_id'], name: 'index_suggestions_on_package_id', unique: true
+  create_table "suggestions", force: :cascade do |t|
+    t.date "updated_at"
+    t.bigint "package_id", null: false
+    t.index ["package_id"], name: "index_suggestions_on_package_id", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email'
-    t.date 'updated_at'
-    t.index ['name'], name: 'index_users_on_name', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email"
+    t.date "updated_at"
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
-  create_table 'versions', force: :cascade do |t|
-    t.string 'number', null: false
-    t.string 'title'
-    t.text 'description'
-    t.string 'r_version'
-    t.string 'license'
-    t.date 'published_at'
-    t.date 'updated_at'
-    t.bigint 'package_id', null: false
-    t.index %w[number package_id], name: 'index_versions_on_number_and_package_id', unique: true
-    t.index ['package_id'], name: 'index_versions_on_package_id'
+  create_table "versions", force: :cascade do |t|
+    t.string "number", null: false
+    t.string "title"
+    t.text "description"
+    t.string "r_version"
+    t.string "license"
+    t.date "published_at"
+    t.date "updated_at"
+    t.bigint "package_id", null: false
+    t.index ["number", "package_id"], name: "index_versions_on_number_and_package_id", unique: true
+    t.index ["package_id"], name: "index_versions_on_package_id"
   end
 
-  add_foreign_key 'authors', 'users', on_delete: :cascade
-  add_foreign_key 'authors', 'versions', on_delete: :cascade
-  add_foreign_key 'dependencies', 'packages', on_delete: :cascade
-  add_foreign_key 'maintainers', 'users', on_delete: :cascade
-  add_foreign_key 'maintainers', 'versions', on_delete: :cascade
-  add_foreign_key 'suggestions', 'packages', on_delete: :cascade
-  add_foreign_key 'versions', 'packages', on_delete: :cascade
+  add_foreign_key "authors", "users", on_delete: :cascade
+  add_foreign_key "authors", "versions", on_delete: :cascade
+  add_foreign_key "dependencies", "packages", on_delete: :cascade
+  add_foreign_key "maintainers", "users", on_delete: :cascade
+  add_foreign_key "maintainers", "versions", on_delete: :cascade
+  add_foreign_key "suggestions", "packages", on_delete: :cascade
+  add_foreign_key "versions", "packages", on_delete: :cascade
 end
